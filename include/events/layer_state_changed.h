9de436b53dbc62acd9ba3a64b34a27cea3c759d7
Takahiro Natsume <tkn.infomail@gmail.com>
2025-07-08 22:07:38 +0900

    add new layer state

--- include/events/layer_state_changed.h
+++ include/events/layer_state_changed.h
@@ -12,6 +12,12 @@ struct layer_state_changed {
 
 ZMK_EVENT_DECLARE(layer_state_changed);
 
+struct layer_state_changed *new_layer_state_changed(void) {
+    struct layer_state_changed *ev = k_malloc(sizeof(struct layer_state_changed));
+    ev->header.last_listener_index = 0;
+    return ev;
+}
+
 static inline struct layer_state_changed *create_layer_state_changed(uint8_t layer, bool state) {
     struct layer_state_changed *ev = new_layer_state_changed();
     ev->layer = layer;
